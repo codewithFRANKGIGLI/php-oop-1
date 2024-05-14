43e5bf0c0d674f3f59cae5ac1d8bf3069d85962b
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-05-14 16:44:18 +0200

    feat: bonus 1 e 2

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+// Array di istanze o oggetti
+$movies = [
+    new Movie('The Matrix', ['Action'], 1999),
+    new Movie('The Godfather', ['Drama', 'Crime'], 1972),
+    new Movie('Pulp Fiction', ['Crime', 'Drama'], 1994),
+];
+
+
+?>
--- index.php
+++ index.php
@@ -1,33 +1,62 @@
 <?php
-class Movie {
+// Collego il file db.php con l'array di films
+require_once __DIR__ . '/db.php';
+
+class Movie
+{
     // Dichiaro le variabili
     public $title;
     public $year;
     public $genre;
 
     // Dichiaro il costruttore
-    public function __construct($title, $genre, $year) {
+    public function __construct($title, $genre, $year)
+    {
         $this->title = $title;
         $this->year = $year;
         $this->genre = $genre;
     }
 
     // Creo un metodo per ottenere ogni variabile
-    public function getTitle() {
+    public function getTitle()
+    {
         return $this->title;
     }
-    public function getGenre() {
+    public function getGenre()
+    {
         return $this->genre;
-    } 
-    public function getYear() {
+    }
+    public function getYear()
+    {
         return $this->year;
     }
-
 }
 
 // Creo un instanza di 'Movie':
-$movie1 = new Movie("The Matrix", "Action", 1999);
-
-
-
+// $movie1 = new Movie("The Matrix", "Action", 1999);
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Movies</title>
+</head>
+
+<body>
+    <h1>Movies</h1>
+    <ul>
+        <?php foreach ($movies as $movie) { ?>
+            <li>
+                <?= $movie->getTitle() ?> (<?= $movie->getYear() ?>)
+                <ul>
+                    <?php foreach ($movie->getGenre() as $genre) { ?>
+                        <li><?= $genre ?></li>
+                    <?php } ?>
+                </ul>
+            </li>
+        <?php } ?>
+    </ul>
+</body>
+
+</html>
